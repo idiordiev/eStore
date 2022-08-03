d1c68ff8c24db6da3ac208abf62d9cce40ff7b80
Ivan Diordiev <74953631+idiordiev@users.noreply.github.com>
2022-08-03 19:15:14 +0300

    added identityDbContext

---
+++ eStore.Infrastructure/Identity/IdentityContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eStore.Infrastructure.Identity
+{
+    public class IdentityContext : IdentityDbContext
+    {
+        public IdentityContext()
+        {
+            
+        }
+        
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(
+                    @"Server=localhost\sqlexpress;Database=eStore_Identity;Trusted_Connection=True;MultipleActiveResultSets=True");
+        }
+    }
+}
