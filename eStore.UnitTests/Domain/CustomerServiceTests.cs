d35103a3798fbf80396fc36bfe7ed32dfb281558
Ivan Diordiev <74953631+idiordiev@users.noreply.github.com>
2022-08-07 14:50:46 +0300

    added fixtures for domain services tests

---
+++ eStore.UnitTests/Domain/CustomerServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace eStore.UnitTests.Domain
+{
+    [TestFixture]
+    public class CustomerServiceTests
+    {
+        
+    }
+}
---
+++ eStore.UnitTests/Domain/GoodsServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eStore.ApplicationCore.Entities;
+using eStore.ApplicationCore.Enums;
+using eStore.ApplicationCore.FilterModels;
+using eStore.ApplicationCore.Services;
+using eStore.Infrastructure.Data;
+using eStore.Infrastructure.Data.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace eStore.UnitTests.Domain
+{
+    [TestFixture]
+    public class GoodsServiceTests
+    {
+        
+    }
+}
