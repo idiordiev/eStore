4ad9b9d2656a3144762a0eebb7682a51eaa2264e
Ivan Diordiev <74953631+idiordiev@users.noreply.github.com>
2022-08-07 14:50:15 +0300

    added goods filter model

---
+++ eStore.ApplicationCore/FilterModels/GoodsFilterModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace eStore.ApplicationCore.FilterModels
+{
+    public abstract class GoodsFilterModel
+    {
+        public ICollection<int> ManufacturerIds { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+    }
+}
