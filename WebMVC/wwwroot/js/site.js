5bd984c7733256235900c4542d31d0a6d89b81d2
Ivan Diordiev <74953631+idiordiev@users.noreply.github.com>
2022-10-21 13:23:50 +0300

    refactored front

--- WebMVC/wwwroot/js/site.js
+++ WebMVC/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleFilter() {
+    let sidebar = document.getElementById("filter-sidebar");
+    if (sidebar.style.left === "0px")
+        sidebar.style.left = "-180px";
+    else
+        sidebar.style.left = "0px";
+}
+
+let myForm = document.getElementById('filter-form');
+myForm.addEventListener('submit', function () {
+    let allInputs = myForm.getElementsByTagName('input');
+    for (let i = 0; i < allInputs.length; i++) {
+        let input = allInputs[i];
+        if (input.name && !input.value) {
+            input.name = '';
+        }
+    }
+});
